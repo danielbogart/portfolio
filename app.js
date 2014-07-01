9fb83fa1ce00bc14a6cb152ad492dd03c0ccfbd6
Daniel Bogart <daniel.e.bogart@gmail.com>
2014-06-30 17:03:24 -0700

    Contact section created

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
 
+	var myLatlng = new google.maps.LatLng(32.802341, -117.238873);
+	var mapOptions = {
+	  zoom: 15,
+	  center: myLatlng,
+	  scrollwheel: false
+	}
 
+	//initialize google maps, centered on myLatLng
+	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 })
